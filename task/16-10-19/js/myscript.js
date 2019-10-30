22f1ecc1665b20b8b2b831af303fd9b634e7ba56
ayushbgi <ayush@businessgateways.com>
2019-10-30 14:41:19 +0530

    my

---
+++ task/16-10-19/js/myscript.js
@@ -0,0 +1,28 @@
+
+var a = 10;
+var b = 20;
+var c = 30;
+
+c += b;
+
+i++;
+
+i+1;
+
+function display() {
+	//document.write("<b>Function Call : " + printtext + "</b>");
+	alert('funtion called');
+var printtext = 'World!';
+printtext.length;
+var printtext = prompt('please enter value', 'World!');
+if(printtext) {
+	alert('user entered value')
+	document.getElementById('disp').innerHTML = "<b>Function Call : " + printtext + "</b>";
+} else {
+	alert('user refused to enter value')
+}
+	//document.getElementByTag('disp').innerHTML = "<b>Function Call : " + printtext + "</b>";
+	//document.getElementByName('disp').innerHTML = "<b>Function Call : " + printtext + "</b>";
+	//document.getElementByClass('disp').innerHTML = "<b>Function Call : " + printtext + "</b>";
+}
+
